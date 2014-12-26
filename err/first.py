5327b75a1d8ec929494657522418ca53bd735735
benniuo <1962323664@qq.com>
2014-12-26 10:57:21 +0800

    add multiple exception

--- err/first.py
+++ err/first.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-x = input("Enter the first number: ")
-y = input("Enter the sencond number: ")
-
 try:
+	x = input("Enter the first number: ")
+	y = input("Enter the sencond number: ")
 	print x/y
 except ZeroDivisionError:
 	print "the sencond number can't be zero!"
+except TypeError, e:
+	print "That wasn't a number,was it?"
 
 print 'End'
