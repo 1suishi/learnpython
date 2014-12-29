51515c05816002063628304b330c5a45fceaddb9
benniuo <1962323664@qq.com>
2014-12-29 10:48:47 +0800

    add the err deal

--- io/testjson.py
+++ io/testjson.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+
+# can't name my own file json.py
+# because there is a module named json
+# this will cause the module can't be import
+
 import json
 d = dict(name = "Bob" , age ="20", score = 88)
 print json.dumps(d)
