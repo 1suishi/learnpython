852580f4041a8a967b5c4dac040227eac7218820
benniuo <1962323664@qq.com>
2014-12-31 14:49:52 +0800

    add parameter

--- decorator/single.py
+++ decorator/single.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
-def salesgirl(method):
-	def serve(*args):
-		print 'Salegirl : Hello, what do you want?',method.__name__
-		result = method(*args)
-		if result:
-			print "Salesgirl: This shirt is 50$."
-		else:
-			print "Salesgirl: Well, how about trying another style?"
-		return result
-	return serve
+def salesgirl(discount):
+	def expense(method):
+		def serve(*args):
+			print 'Salegirl : Hello, what do you want?',method.__name__
+			result = method(*args)
+			if result:
+				print "Salesgirl: This shirt is 50$.As an old user,we promised to discount at %d%%" % (discount)
+			else:
+				print "Salesgirl: Well, how about trying another style?"
+			return result
+		return serve
+	return expense
 
-@salesgirl
+@salesgirl(50)
 def try_this_shirt(size):
     if size < 35:
         print "I: %d inches is to small to me" %(size)
