9ca2a98fd87cf32429651b6637fe037d65cbcc69
benniuo <1962323664@qq.com>
2014-12-31 14:43:48 +0800

    decotorator

--- decorator/test_decorator.py
+++ decorator/test_decorator.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
+
 import time
 
+
+def log(func):
+	def wrapper(*args,**kw):
+		print "call begin"
+		var =  func(*args , **kw)
+		print "call end"
+		return var
+	return wrapper
+
+
+@log
 def now():
 	return time.time()
 
 print now()
-print now.__name__
+
+
